cf9b328fead8d6a549fa8a495a849450df474e5c
dalianazp <daliana.zambrano@epn.edu.ec>
2021-08-21 09:35:26 -0500

    Linkiar el Botón Continuar del registro del AM

--- app/src/main/java/com/example/findbue/RegistrarAdultoMayor.java
+++ app/src/main/java/com/example/findbue/RegistrarAdultoMayor.java
@@ -2,10 +2,14 @@ package com.example.findbue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class RegistrarAdultoMayor extends AppCompatActivity {
+    Button continuar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,14 @@ public class RegistrarAdultoMayor extends AppCompatActivity {
         //Ocultamos la barra de estado del sistema
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_registrar_adulto_mayor);
+
+        continuar = (Button) findViewById(R.id.button6);
+        continuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrarAdultoMayor.this, RegUbiAdultoMayor.class);
+                startActivity(intent);
+            }
+        });
     }
 }
