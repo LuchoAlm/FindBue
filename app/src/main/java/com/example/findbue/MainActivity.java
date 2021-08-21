1e7c462a96d2c55df3c48fba8214513ca275f1ff
LuchoAlm <srs_alm@hotmail.com>
2021-08-21 05:44:28 -0500

    Interfaz de login creada, falta diseño

--- app/src/main/java/com/example/findbue/MainActivity.java
+++ app/src/main/java/com/example/findbue/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.findbue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Ocultamos la barra de acción
+        getSupportActionBar().hide();
+        //Ocultamos la barra de estado del sistema
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+
         setContentView(R.layout.activity_main);
     }
 }
