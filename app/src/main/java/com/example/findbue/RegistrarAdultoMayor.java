ad9c5d26a470481c12e1c769d29d53fbe273af33
LuchoAlm <srs_alm@hotmail.com>
2021-08-21 07:00:41 -0500

    Interfaz de RegistrarAdultoMayor 1/2 completa

---
+++ app/src/main/java/com/example/findbue/RegistrarAdultoMayor.java
@@ -0,0 +1,19 @@
+package com.example.findbue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class RegistrarAdultoMayor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Ocultamos la barra de acción
+        getSupportActionBar().hide();
+        //Ocultamos la barra de estado del sistema
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        setContentView(R.layout.activity_registrar_adulto_mayor);
+    }
+}
