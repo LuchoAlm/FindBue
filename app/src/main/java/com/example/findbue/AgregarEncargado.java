aa46019538c6aa57e27c671cec2a99971a5b8d12
LuchoAlm <srs_alm@hotmail.com>
2021-08-23 09:51:38 -0500

    Agregada la interfaz de Agregar Encargado

---
+++ app/src/main/java/com/example/findbue/AgregarEncargado.java
@@ -0,0 +1,14 @@
+package com.example.findbue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarEncargado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_encargado);
+    }
+}
